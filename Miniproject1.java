931d869d6d54e73c8239bf9dda4a6176488fbd65
Shazia Afreen <166709231+Shafreen123@users.noreply.github.com>
2025-08-02 11:34:27 +0530

    Add files via upload

---
+++ Miniproject1.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.miniproject1;
+
+import com.mycompany.miniproject1.SignUp;
+
+/**
+ *
+ * @author HP
+ */
+public class Miniproject1 {
+
+    public static void main(String[] args) {
+        User u= new User();
+        u.setVisible(true);
+        
+        // Home h = new Home();
+        //SignUp s = new SignUp();
+        // explore e = new explore();
+    }
+}
